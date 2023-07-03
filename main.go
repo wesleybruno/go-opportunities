90cb612bd89fe19d7e29b6a0416b7fb3053c4f21
Wesley Oliveira <wesleyoliveira@cronosbank.com.br>
2023-07-03 16:52:34 -0300

    feat: config module

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"github.com/LagLabs/backend-go.git/config"
 	"github.com/LagLabs/backend-go.git/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config init error: %v", err)
+		return
+	}
+	//Initialize router
 	router.Initialize()
 }
